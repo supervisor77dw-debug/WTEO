ca82b5b5f1283ede7425c10c2f02c44c8a21d85b
Dirk Wilkens <supervisor77.dw@googlemail.com>
2025-12-11 17:12:14 +0100

    fix: include /style.css in generated pages and root index

--- generate_pages.js
+++ generate_pages.js
@@ -447,6 +447,7 @@ const getTemplate = (data, pth) => {
     <meta charset="UTF-8" />
     <link rel="icon" type="image/svg+xml" href="/vite.svg" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="/style.css" />
     <title>${data.title} | WTEO</title>
   </head>
   <body>
